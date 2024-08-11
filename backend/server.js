c826b09eb8a1efc61cd484de4a154ca0c06be4ea
ujala <singhujala2004@gmail.com>
2024-08-11 21:08:46 +0530

    "Updated .env file, added PORT variable and modified server.js to serve frontend dist files; updated frontend package.json and package-lock.json to include application-tracker dependency; updated main script in package.json"

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import authRoute from "./Routes/authRoute.js"
 import jobRoute from "./Routes/jobRoute.js"
 import updateAnddeleteRoute from "./Routes/updateAnddeleteRoute.js"
 import cookieParser from "cookie-parser"
+import path from "path"
 dotenv.config()
 
 mongoose
@@ -27,7 +28,16 @@ app.use(cors(
 
 const port = process.env.PORT || 5000
 
+const __dirname = path.resolve();
+
 app.use("/api/auth", authRoute)
 app.use("/api/job", jobRoute)
 app.use("/api/u&d", updateAnddeleteRoute)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname, "frontend", "dist" ,"index.html"))
+})
+
 app.listen(port, () => console.log(`server listening on port :${port}`))
